b390b1c8008c032a013544f7264486d14fb23427
Gaya <91261240+sdooodly@users.noreply.github.com>
2025-04-03 05:48:52 +0000

    basic fw

---
+++ crud_mvc/Models/Person.cs
@@ -0,0 +1,10 @@
+namespace crud_mvc.Models
+{
+    public class Person
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Email {get; set;}
+        public string PhoneNumber {get; set;}
+    }
+}
