245c8ac53d3af4a3ad137070a66ec846c31af3d3
Gayathri Gireesh Sujatha <gayathri.gireeshsujatha@nielsen.com>
2025-04-15 08:58:49 +0530

    add comments

---
+++ crud_mvc/Migrations/20250414110614_AddFavLanguage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace crud_mvc.Migrations
+{
+    public partial class AddFavLanguage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FavLanguage",
+                table: "People",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FavLanguage",
+                table: "People");
+        }
+    }
+}
--- crud_mvc/Models/Person.cs
+++ crud_mvc/Models/Person.cs
@@ -1,7 +1,15 @@
+// here we define a BLUEPRINT/CLASS for a person
+// and this class has properties like id and name etc
+// this is PUBLIC
 namespace crud_mvc.Models
 {
     public class Person
     {
+        // public : access modifier
+        // string : data type
+        // Name - property is named in PascalCase way
+        // get and set are accessors of this property, getter and setter
+        // empty intitalises an empty value in the beginning. this ensures it will always have a default value
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
